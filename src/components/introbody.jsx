ab2de6a010905f967a89970295840921de7374ff
Amir <amirhoosindoosti15@gmail.com>
2023-12-14 14:23:08 +0330

    minor changes

--- src/components/introbody.jsx
+++ src/components/introbody.jsx
@@ -3,7 +3,7 @@ function Introbody({ year, title, link, imgsrc, a_tag }) {
   return (
     <>
       <div className="flex flex-col gap-5 items-center justify-center">
-        <h2 className="md:text-xl font-semibold -mb-2 mt-2 bg-white p-1 rounded-lg md:w-56 w-40 text-center">{title}</h2>
+        <h2 className="md:text-xl font-semibold -mb-2 mt-4 border-2 border-black bg-white p-1 rounded-lg md:w-56 w-40 text-center">{title}</h2>
         <img
           src={imgsrc}
           alt={title}
@@ -13,9 +13,9 @@ function Introbody({ year, title, link, imgsrc, a_tag }) {
           {year}
         </h2>
         <h2>
-          <a href={link} className="btn-blue bg-emerald-900 font-medium">{title} {a_tag}</a>
+          <a href={link} target="_blank" className="btn-blue bg-emerald-900 font-medium">{title} {a_tag}</a>
         </h2>
-        <hr className="w-full h-0.5 border-none bg-emerald-400" />
+        <hr className="w-2/3 md:w-1/2 h-0.5 border-none bg-amber-400 my-3" />
       </div>
     </>
   );
--- src/components/mainbody.jsx
+++ src/components/mainbody.jsx
@@ -4,7 +4,7 @@ function Main() {
   function clickon() {
   let i = 0;
   i++;
-  const like = document.getElementById("button").innerHTML = "❤ = " + i;
+  const like = document.getElementById("button").innerHTML = "❤  " + i;
   alert("Thank You :)");
   if (i > 0){
     document.getElementById("button").disabled = true;
@@ -14,10 +14,10 @@ function Main() {
     <>
       <div className="flex items-center justify-center flex-row">
         <h2 className="md:text-xl my-2 text-white bg-emerald-900 p-2 rounded">
-          My Top 5 <span className="underline text-amber-400">Mini</span>{" "}
+          My Top 5 <span className="underline text-amber-400">Best</span>{" "}
           Projects
         </h2>
-        <button className="btn-blue ml-4 font-normal bg-red-600 hover:bg-red-800 p-1" onClick={clickon} id="button">❤ = 0</button>
+        <button className="btn-blue ml-4 font-normal bg-red-600 hover:bg-red-800 p-1" onClick={clickon} id="button">❤ 0</button>
       </div>
     </>
   );
